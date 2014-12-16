379d5dbc0747948f40f93a7bec100862cc23f6ad
shiguang <diabloboy@163.com>
2014-12-16 10:21:52 +0800

    增加添加页面的封装类

---
+++ danny-common/src/main/java/net/sigon/danny/common/generate/factory/generator/AddPageGenerator.java
@@ -0,0 +1,11 @@
+package net.sigon.danny.common.generate.factory.generator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sigon
+ * Date: 14-12-15
+ * Time: 下午9:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class AddPageGenerator {
+}
