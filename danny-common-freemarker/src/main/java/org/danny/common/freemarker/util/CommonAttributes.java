5c1a9ee568d67268e53a24546a15f13cdd57cff9
shiguang <shiguang@sigon.me>
2017-02-02 20:07:30 +0800

    增加分页模块

---
+++ danny-common-freemarker/src/main/java/org/danny/common/freemarker/util/CommonAttributes.java
@@ -0,0 +1,10 @@
+package org.danny.common.freemarker.util;
+
+public final class CommonAttributes {
+
+	public static final String[] DATE_PATTERNS = new String[] { "yyyy", "yyyy-MM", "yyyyMM", "yyyy/MM", "yyyy-MM-dd", "yyyyMMdd", "yyyy/MM/dd", "yyyy-MM-dd HH:mm:ss", "yyyyMMddHHmmss", "yyyy/MM/dd HH:mm:ss" };
+
+	private CommonAttributes() {
+	}
+
+}
---
+++ danny-common-util/src/main/java/org/danny/common/util/EnumConverter.java
@@ -0,0 +1,33 @@
+package org.danny.common.util;
+
+import org.apache.commons.beanutils.converters.AbstractConverter;
+/**
+ * Created by Administrator on 2016/11/18.
+ */
+public class EnumConverter extends AbstractConverter{
+    private final Class<?> enumClass;
+
+    public EnumConverter(Class<?> enumClass) {
+        this(enumClass, null);
+    }
+
+    public EnumConverter(Class<?> enumClass, Object defaultValue) {
+        super(defaultValue);
+        this.enumClass = enumClass;
+    }
+
+    @Override
+    protected Class<?> getDefaultType() {
+        return this.enumClass;
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    protected Object convertToType(Class type, Object value) {
+        String stringValue = value.toString().trim();
+        return Enum.valueOf(type, stringValue);
+    }
+
+    protected String convertToString(Object value) {
+        return value.toString();
+    }
+}
